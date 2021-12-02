ca7df27085ac7183fff4b5f94b9d2e84645e2dac
TNT43 <thienboybt@gmail.com>
2021-12-02 21:33:23 +0700

    Create function for count word in page

--- main.py
+++ main.py
@@ -3,4 +3,17 @@ from bs4 import BeautifulSoup
 all_link = []       # Suitable link list
 all_search = []     # Search in single link
 
+
+def count_word(string1, string2):
+    count = 0
+    index_find = -1
+    for i in range(len(string1)):
+        count += 1
+        index_find = string1.lower().find(string2.lower(), index_find+1)
+        if index_find == -1:
+            count -= 1
+            break
+    return count
+
+
 print("Code done!!!")
